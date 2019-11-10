770946a905b29ce86642e316303753609d872475
Junip Dewan <junipd2@gmail.com>
2019-11-10 10:23:22 +0530

    first cut for repo switch

---
+++ src/dir.js
@@ -0,0 +1,28 @@
+/**
+ * Directory related APIs
+ * i.e Add/Remove/
+ */
+
+module.exports = {
+  /**
+   * Search for the directory
+   */
+  searchDirectory: function() {},
+
+  addDirectoryPath: function() {},
+
+  removeDirectoryPath: function() {},
+
+  editDirectoryPath: function() {},
+
+  /**
+   * Check if the directory path is valid or not
+   */
+
+  checkValidPath: function() {},
+
+  /**
+   * Execute the given Path
+   */
+  goToPath: function(path) {}
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,55 @@
-const program = require('commander');
+#!/usr/bin/env node
+const program = require("commander");
+const inquirer = require("inquirer");
+const inquirerAutoPrompt = require("inquirer-autocomplete-prompt");
+const Configstore = require("configstore");
+var shell = require("shelljs");
+const configStore = new Configstore("dirpathdetail");
+
 program
-  .option('-s, --switch','Switch Repo')
-  .option('-a, --add','Add Repo')    
+  .option("-d, --directory", "Switch Repo")
+  .option("-a, --add-directory", "Add Directory")
+  .option("-e, --edit-directory", "Edit Directory Path");
+
+program.parse(process.argv);
+
+if (program.directory) {
+}
+
+if (program.addDirectory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "dir_name",
+        message: "Enter the Name of Directory you want to add",
+        validate: function(value) {
+          return value.length ? true : "Please enter the directory Name ";
+        }
+      },
+      {
+        type: "input",
+        name: "path_name",
+        message: "Add your directory Path",
+        validate: function(value) {
+          return value.length ? true : `Please enter valid directory path`;
+        }
+      }
+    ])
+    .then(answers => {
+      let result = JSON.stringify(answers, null, "  ");
+      let path_name, dir_name;
+      path_name = result.path_name;
+      dir_name = result.dir_name;
+      debugger
+      console.log(dir_name, path_name)
+      configStore.set({dir_name: path_name});
+      //Users/admin/Documents/iserve
+      // if (shell.test("-d", result.path_name)) {
+        
+      // } else {
+      //   console.log("Please Enter a valid directory or directory path");
+      // }
+      console.log(configStore.all)
+    });
+}
